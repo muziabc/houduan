5d3aaf05f431bc1f9d42563da223ffd4ac8d9c9a
muziabc <52307110+muziabc@users.noreply.github.com>
2019-07-22 14:23:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd `dirname $0`
 
-# 项目构建操作....
 echo "使用 Docker-Maven 打包项目"
 docker run --rm \
    -v ~/.m2:/root/.m2 \
@@ -9,7 +7,8 @@ docker run --rm \
    -w /usr/src/mymaven \
    maven:3.3.3-jdk-8 mvn clean package -U
 
-mv $PROJECT_HOME/*-provider/target/*-provider-*.jar $PROJECT_HOME/*-provider/target/demo.jar
+mv $PROJECT_HOME/*-provider/target/*-provider-*.jar $PROJECT_HOME/deepexi-provider/target/demo.jar
 
 echo "构建镜像"
 docker build -t $APP_NAME:v$VERSION .
+
